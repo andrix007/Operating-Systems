82676110fe0e5dc1010ea660e10f57d6c755a1cf
andrei <ardut2004@gmail.com>
2023-09-14 21:56:03 +0200

    Assignment 1.1

---
+++ getCleanCode/cleancode.py
@@ -0,0 +1,16 @@
+import json
+import pyperclip
+
+with open("outputCode.txt", "w") as f1:
+    with open("input.txt", "r") as f:
+        for line in f.readlines():
+            pos = 0
+            for i in range(len(line)):
+                if not line[i].isdigit():
+                    pos = i
+                    break
+            line = line[pos:]
+            f1.write(line)
+
+with open("outputCode.txt", "r") as f:
+    pyperclip.copy(f.read())
